d4a46766e620d1226b0cab97a4d557b9bda37766
Remi FRISCCA <rfriscca@e2r1p8.42.fr>
2018-01-11 11:12:48 +0100

    update Colleen.c and Makefile

--- Colleen/Colleen.c
+++ Colleen/Colleen.c
@@ -7,12 +7,7 @@
 int my_func() {return(0);}
 
 int main() {
-char *s = "#include <stdio.h>%c%c/*%c%cCeci est un commentaire en dehors \
-de la fonction main%c*/%cint my_func() {return(0);}%c%c\
-int main() {%cchar *s =%c%s%c;%c/*%c%cCeci est un commentaire dans la \
-fonction main%c*/%cint a;%ca = my_func();%c\
-printf(s, 10, 10, 10, 9, 10, 10, 10 , 10, 10, 10, 10, 34, s, 34, 10, 10, 9, 10, 10, 10, 10, 10, 10);\
-%creturn(0);%c}" ;
+char *s = "#include <stdio.h>%c%c/*%c%cCeci est un commentaire en dehors de la fonction main%c*/%cint my_func() {return(0);}%c%cint main() {%cchar *s =%c%s%c;%c/*%c%cCeci est un commentaire dans la fonction main%c*/%cint a;%ca = my_func();%cprintf(s, 10, 10, 10, 9, 10, 10, 10 , 10, 10, 10, 10, 34, s, 34, 10, 10, 9, 10, 10, 10, 10, 10, 10);%creturn(0);%c}" ;
 /*
 	Ceci est un commentaire dans la fonction main
 */
