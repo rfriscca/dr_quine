995b6468259d38b514ffe65e621fd2a4febdeba3
Remi FRISCCA <rfriscca@e2r1p7.42.fr>
2018-01-12 11:25:11 +0100

    comment in Grace.c

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-#define print_to_file(x, y) fprintf(x, y, 10, 10, 34, 34, 34, 34, 10, 34, y, 34, 10);
+#define print_to_file(x, y) fprintf(x, y, 10, 10, 34, 34, 34, 34, 10, 34, y, 34, 10, 10, 9, 10, 10);
 #define open_file FILE* fptr = fopen("Grace_kid.c", "w");
-#define F() int main() {open_file print_to_file(fptr, "#include <stdio.h>%c#define print_to_file(x, y) fprintf(x, y, 10, 10, 34, 34, 34, 34, 10, 34, y, 34, 10);%c#define open_file FILE* fptr = fopen(%cGrace_kid.c%c, %cw%c);%c#define F() int main() {open_file print_to_file(fptr, %c%s%c)}%cF()")}
+#define F() int main() {open_file print_to_file(fptr, "#include <stdio.h>%c#define print_to_file(x, y) fprintf(x, y, 10, 10, 34, 34, 34, 34, 10, 34, y, 34, 10, 10, 9, 10, 10);%c#define open_file FILE* fptr = fopen(%cGrace_kid.c%c, %cw%c);%c#define F() int main() {open_file print_to_file(fptr, %c%s%c)}%c/*%c%ccommentaire%c*/%cF()")}
+/*
+	commentaire
+*/
 F()
